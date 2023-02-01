9edb59ae2064f4694b49417470774e155c53a210
proohit <direnc99@gmail.com>
2023-02-01 18:52:45 +0100

    refactor: request handling for readability

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,7 @@
 use bytecodec::DecodeExt;
 use httpcodec::{HttpVersion, ReasonPhrase, Request, RequestDecoder, Response, StatusCode};
+use std::io::{Read, Write};
 use wasmedge_wasi_socket::{Shutdown, TcpListener, TcpStream};
-
-fn handle_http(req: Request<String>) -> bytecodec::Result<Response<String>> {
-    Ok(Response::new(
-        HttpVersion::V1_0,
-        StatusCode::new(200)?,
-        ReasonPhrase::new("")?,
-        format!("echo: {}", req.body()),
-    ))
-}
-
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut buff = [0u8; 1024];
-    let mut data = Vec::new();
-
-    loop {
-        let n = stream.read(&mut buff)?;
-        data.extend_from_slice(&buff[0..n]);
-        if n < 1024 {
-            break;
-        }
-    }
-
-    let mut decoder =
-        RequestDecoder::<httpcodec::BodyDecoder<bytecodec::bytes::Utf8Decoder>>::default();
-
-    let req = match decoder.decode_from_bytes(data.as_slice()) {
-        Ok(req) => handle_http(req),
-        Err(e) => Err(e),
-    };
-
-    let r = match req {
-        Ok(r) => r,
-        Err(e) => {
-            let err = format!("{:?}", e);
-            Response::new(
-                HttpVersion::V1_0,
-                StatusCode::new(500).unwrap(),
-                ReasonPhrase::new(err.as_str()).unwrap(),
-                err.clone(),
-            )
-        }
-    };
-
-    let write_buf = r.to_string();
-    stream.write(write_buf.as_bytes())?;
-    stream.shutdown(Shutdown::Both)?;
-    Ok(())
-}
 
 #[no_mangle]
 fn start() {
@@ -60,16 +13,67 @@ fn start() {
     }
 }
 
-use std::env;
-use std::fs;
-use std::fs::File;
-use std::io::{Read, Write};
-#[no_mangle]
-fn test() {
-    let mut file = File::open("test.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    println!("{}", contents);
-    let mut file = File::create("test.txt").unwrap();
-    file.write_all("hello world".as_bytes()).unwrap();
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    let request_data = read_stream_data(&mut stream)?;
+
+    let response = handle_request(request_data);
+
+    let write_buf = response.to_string();
+    stream.write(write_buf.as_bytes())?;
+    stream.shutdown(Shutdown::Both)?;
+    Ok(())
+}
+
+fn read_stream_data(stream: &mut TcpStream) -> std::io::Result<Vec<u8>> {
+    let mut buff = [0u8; 1024];
+    let mut data = Vec::new();
+
+    loop {
+        let n = stream.read(&mut buff)?;
+        data.extend_from_slice(&buff[0..n]);
+        if n < 1024 {
+            break;
+        }
+    }
+    Ok(data)
+}
+
+fn handle_request(request_data: Vec<u8>) -> Response<String> {
+    let mut decoder =
+        RequestDecoder::<httpcodec::BodyDecoder<bytecodec::bytes::Utf8Decoder>>::default();
+
+    let raw_response = match decoder.decode_from_bytes(request_data.as_slice()) {
+        Ok(req) => respond_request(req),
+        Err(e) => Err(e),
+    };
+
+    match raw_response {
+        Ok(r) => r,
+        Err(e) => {
+            let err = format!("{:?}", e);
+            Response::new(
+                HttpVersion::V1_0,
+                StatusCode::new(500).unwrap(),
+                ReasonPhrase::new(err.as_str()).unwrap(),
+                err.clone(),
+            )
+        }
+    }
+}
+
+fn respond_request(req: Request<String>) -> bytecodec::Result<Response<String>> {
+    let path = req.request_target();
+    Ok(Response::new(
+        HttpVersion::V1_0,
+        StatusCode::new(200)?,
+        ReasonPhrase::new("")?,
+        format!(
+            "Hello world from WebAssembly!
+
+Path: {},
+Body: {}",
+            path,
+            req.body()
+        ),
+    ))
 }
