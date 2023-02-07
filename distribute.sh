09615d06bbcfdba3ba73e15631d91b952d9614d4
proohit <direnc99@gmail.com>
2023-02-07 17:41:30 +0100

    feat: java bindings and example

--- distribute.sh
+++ distribute.sh
@@ -2,3 +2,4 @@ cargo build --target=wasm32-wasi --release
 WASM_FILE="target/wasm32-wasi/release/custom_framework_wasm.wasm"
 cp $WASM_FILE ./bindings/go/lib
 cp $WASM_FILE ./bindings/rust/lib
+cp $WASM_FILE ./bindings/java/custom_framework/src/main/resources
